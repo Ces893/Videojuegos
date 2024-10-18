7eb4c160a5d6bb7313cc32efbe49ed4f1a693259
Cesar Andree <cesar-andree-2003@hotmail.com>
2024-10-18 17:42:58 -0500

    T2

--- Assets/Scrips/Canvas.cs
+++ Assets/Scrips/Canvas.cs
@@ -1,28 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Canvas : MonoBehaviour
 {
     public GameObject[] vidas;
+    public TextMeshProUGUI TextMeshProUGUI;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){
 
+    }
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        TextMeshProUGUI.text = "Monedas : " + GameManager.Instance.MonedasTotales.ToString();
     }
-
-    public void DesactivarVida(int indice) {
+    public void DesactivarVida(int indice){
         vidas[indice].SetActive(false);
     }
-
-    public void ActivarVida(int indice) {
+    public void ActivarVida(int indice){
         vidas[indice].SetActive(true);
     }
-    
+
 }
--- Assets/Scrips/GameManager.cs
+++ Assets/Scrips/GameManager.cs
@@ -8,30 +8,44 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public Canvas canvas;
     private int vidas = 3;
-
+    public int MonedasTotales { get { return monedas; } }
+    private int monedas = 0;
+    //private GameData gData;
+    //private GameDataRepository gDataRepository;
     public void Awake()
     {
-        if (Instance == null) { 
+        if (Instance == null)
+        {
             Instance = this;
+            //gDataRepository = new GameDataRepository();
+            //gData = gDataRepository.LoadGame();
         }
         else
         {
             Debug.Log("Error");
         }
     }
-
-    public void perderVida() { 
+    public void perderVida()
+    {
         vidas -= 1;
-        if (vidas == 0) {
+        if (vidas == 0)
+        {
             SceneManager.LoadScene(2);
         }
+        //gDataRepository.SaveGame(gData);
         canvas.DesactivarVida(vidas);
     }
-
-    public bool RecuperarVida() {
+    public bool RecuperarVida()
+    {
         if (vidas == 3) { return false; }
         canvas.ActivarVida(vidas);
         vidas += 1;
+        //gDataRepository.SaveGame(gData);
         return true;
+    }
+    public void AddCoin(int valor)
+    {
+        monedas += valor;
+        Debug.Log(monedas);
     }
 }
